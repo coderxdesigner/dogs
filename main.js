f916c8e1417c9145ed67c96784e4e06855640e62
Rachel <Rachel.Heneault@gmail.com>
2022-01-22 07:30:42 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+async function start() {
+  const response = await fetch("https://dog.ceo/api/breeds/list/all")
+  const data = await response.json()
+  createBreedList(data.message)
+}
+
+start()
+
+function createBreedList(breedList) {}
