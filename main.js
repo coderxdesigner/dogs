d4efb5d308f836ed179f0bf1093ba16ed5cda72c
Rachel <Rachel.Heneault@gmail.com>
2022-01-22 08:43:25 -0500

    Added slide show and css

--- main.js
+++ main.js
@@ -6,4 +6,43 @@ async function start() {
 
 start()
 
-function createBreedList(breedList) {}
+function createBreedList(breedList) {
+  document.getElementById("breed").innerHTML = `
+    <select onchange="loadByBreed(this.value)"><option>Choose a dog breed</option>
+    ${Object.keys(breedList)
+      .map(function (breed) {
+        return `<option>${breed}</option>`
+      })
+      .join("")}
+    </option></select>
+  `
+}
+async function loadByBreed(breed) {
+  if (breed != "Choose a dog breed") {
+    const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`)
+    const data = await response.json()
+    createSlideShow(data.message)
+  }
+}
+
+function createSlideShow(images) {
+  let currentPosition = 0
+  document.getElementById("slideshow").innerHTML = `
+  <div class="slide" style="background-image:url(${images[0]})"></div>
+  <div class="slide" style="background-image:url(${images[1]})"></div>
+  `
+  currentPosition += 2
+  setInterval(nextSlide, 3000)
+
+  function nextSlide() {
+    document.getElementById("slideshow").insertAdjacentHTML("beforeend", `<div class="slide" style="background-image:url(${images[currentPosition]})"></div>`)
+    setTimeout(function () {
+      document.querySelector(".slide").remove()
+    }, 1000)
+    if (currentPosition + 1 >= images.length) {
+      currentPosition = 0
+    } else {
+      currentPosition += 1
+    }
+  }
+}
